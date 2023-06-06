6315a0563cae85835e7abd75c97410626ec483df
Stiv Berberi <stiv.berberi1@gmail.com>
2023-06-06 17:22:39 -0400

    Single-threaded server simulating slow responses

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use std::{
-    io::{BufRead, BufReader},
+    fs,
+    io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
 };
 
 fn main() {
@@ -14,12 +17,30 @@ fn main() {
 }
 
 fn handle_connection(mut stream: TcpStream) {
+    // BufReader will manage calls to the std::io::Read trait methods for us
     let buf_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    println!("Request: {:#?}", http_request);
+    // Have to match against string slice of request_line since match doesn't
+    // do automatic referencing/dereferencing like equality == does
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            // simulate a slow response
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "hello.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
+
+    let response = format!(
+        "{status_line}\r\n\
+        Content-length: {length}\r\n\r\n\
+        {contents}"
+    );
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
