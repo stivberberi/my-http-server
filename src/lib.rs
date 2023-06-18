5a840ce04c3ff716fe0670649eda9d1defaa2fac
Stiv Berberi <stiv.berberi1@gmail.com>
2023-06-18 17:10:03 -0400

    Multithreaded server working, still requires graceful shutdown

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
-use std::error::Error;
 use std::fmt;
-use std::thread;
+use std::{
+    error::Error,
+    sync::{mpsc, Arc, Mutex},
+    thread,
+};
 
 // custom PoolCreationError
 #[derive(Debug)]
@@ -23,8 +26,11 @@ impl fmt::Display for PoolCreationError {
 impl Error for PoolCreationError {}
 
 pub struct ThreadPool {
-    threads: Vec<thread::JoinHandle<()>>,
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
 }
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
 
 impl ThreadPool {
     /// Create a new ThreadPool.
@@ -36,17 +42,19 @@ impl ThreadPool {
     /// ThreadPool if successful, PoolCreationError otherwise
     /// (size entered is less than 0).
     ///
-    /// # Panics
-    ///
-    /// The `new` function will panic if the size is zero.
     pub fn build(size: usize) -> Result<ThreadPool, PoolCreationError> {
         if size > 0 {
-            let mut threads = Vec::with_capacity(size);
-            for _ in 0..size {
-                todo!("Create some threads");
+            let (sender, receiver) = mpsc::channel();
+            let receiver = Arc::new(Mutex::new(receiver));
+
+            let mut workers = Vec::with_capacity(size);
+
+            for id in 0..size {
+                let worker = Worker::build(id, Arc::clone(&receiver)).unwrap();
+                workers.push(worker);
             }
 
-            Ok(ThreadPool { threads })
+            Ok(ThreadPool { workers, sender })
         } else {
             Err(PoolCreationError::new("Size must be larger than 0."))
         }
@@ -56,5 +64,44 @@ impl ThreadPool {
     where
         F: FnOnce() + Send + 'static,
     {
+        let job = Box::new(f);
+
+        self.sender.send(job).unwrap();
+    }
+}
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn build(
+        id: usize,
+        receiver: Arc<Mutex<mpsc::Receiver<Job>>>,
+    ) -> Result<Worker, PoolCreationError> {
+        let builder = thread::Builder::new();
+        let thread = match builder.spawn(move || loop {
+            // call to recv will block until a task becomes available.
+            // Must use let here (as opposed to if let or while let) so that the
+            // temporary mutex variable is dropped (thus releasing the lock),
+            // allowing for our servor to process requests in parallel.
+            let job = receiver
+                .lock()
+                .expect("Poisoned mutex. Another thread may have panicked.")
+                .recv()
+                .unwrap();
+
+            println!("Worker {id} got a job; executing.");
+            job();
+        }) {
+            Ok(thread) => thread,
+            Err(_) => {
+                return Err(PoolCreationError::new(
+                    "Could not spawn the thread of id: {id}.",
+                ))
+            }
+        };
+        Ok(Worker { id, thread })
     }
 }
